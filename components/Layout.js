8630ad5f3de7382d73dd98726538d8c784a4897e
Fred <floony7@yahoo.com>
2020-08-27 11:10:54 +1200

    updated favicon

--- components/Layout.js
+++ components/Layout.js
@@ -20,7 +20,7 @@ const Layout = ({ children, title }) => {
       <Head>
         <title>{title} | Best Web Templates and WordPress Themes</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css" integrity="sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ==" crossOrigin="anonymous" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
       </Head>
       <Header />
       <main>{children}</main>

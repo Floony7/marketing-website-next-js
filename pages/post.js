2cdbff2993219e7d0fb1758433ca703350caaed1
Fred <floony7@yahoo.com>
2020-08-27 18:42:09 +1200

    More error corrections

--- pages/post.js
+++ pages/post.js
@@ -1,6 +1,6 @@
 // import matter from "gray-matter"
-import ReactMarkdown from "react-markdown"
-const glob = require("glob")
+// import ReactMarkdown from "react-markdown"
+// const glob = require("glob")
 
 const Post = (props) => {
   return (
